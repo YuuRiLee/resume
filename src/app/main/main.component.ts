40548d0890d8c6da606f0c2daff43c6b4b4de758
yurilee9696 <yurilee9696@gmail.com>
2020-04-27 03:50:47 +0900

    ADD resume page 추가

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
@@ -8,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class MainComponent implements OnInit {
+  public data;
 
   constructor(
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
+    this.http.get<any>('./assets/data/resume.json', { observe: 'response' })
+      .subscribe(
+        resp => {
+          this.data = resp.body;
+        },
+        err => { console.log('error' + err.error); }
+      );
   }
 }
