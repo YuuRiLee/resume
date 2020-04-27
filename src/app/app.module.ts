bf97abc9fe0c22cc8ba8ac05e14002b77a7cc125
yurilee9696 <yurilee9696@gmail.com>
2020-04-27 12:02:08 +0900

    MOD img lazy loading and 뒤로가기 시 scroll position 수정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { DetailComponent } from './detail/detail.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PipesModule } from './pipes/pipes.module';
+import { Router, Event, Scroll } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs/operators';
+import { LazyLoadImageModule, scrollPreset } from 'ng-lazyload-image';
 
 @NgModule({
   declarations: [
@@ -23,8 +27,32 @@ import { PipesModule } from './pipes/pipes.module';
     AppRoutingModule,
     HttpClientModule,
     PipesModule,
+    LazyLoadImageModule.forRoot({
+      preset: scrollPreset,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(router: Router, viewportScroller: ViewportScroller) {
+    router.events.pipe(
+      filter((e: Event): e is Scroll => e instanceof Scroll)
+    ).subscribe(e => {
+      if (e.position) {
+        // backward navigation
+        setTimeout(() => {
+          viewportScroller.scrollToPosition(e.position);
+        }, 100);
+        viewportScroller.scrollToPosition(e.position);
+      } else if (e.anchor) {
+        // anchor navigation
+        viewportScroller.scrollToAnchor(e.anchor);
+      } else {
+        // forward navigation
+        // viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
+}
